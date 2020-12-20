98cc5e3f833a5b1c908aec1f848330d18dd02275
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-12-20 23:05:11 +1100

    Added React router dom, started fresh

---
+++ keesydrivingschool/src/components/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import "../App.css";
+import "../Header.css";
+import history from "../history";
+import Header from "./Header";
+import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import Prices from "./pages/Prices";
+import FAQs from "./pages/FAQs";
+import Reviews from "./pages/Reviews";
+import ContactUs from "./pages/ContactUs";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Router history={history}>
+        <div>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/aboutUs" exact component={AboutUs} />
+            <Route path="/prices" exact component={Prices} />
+            <Route path="/faqs" exact component={FAQs} />
+            <Route path="/reviews" exact component={Reviews} />
+            <Route path="/contactUs" exact component={ContactUs} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
--- keesydrivingschool/src/components/Header.js
+++ keesydrivingschool/src/components/Header.js
@@ -1,41 +1,39 @@
-import React, { Component } from "react";
-import Link from "./Link";
+import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 import "../Header.css";
 
-class Header extends Component {
-  render() {
-    return (
-      <div className="main-header">
-        <div className="top-contact-link">
-          0419 421 848 info@keesydrivingschool.com.au
-        </div>
-        <div className="main-header-nav">
-          <img src={logo} alt="logo" />
-
-          <div className="top-menu">
-            <Link href="/" className="">
-              Home
-            </Link>
-            <Link href="/aboutUs" className="">
-              About Us
-            </Link>
-            <Link href="/prices&packages" className="">
-              Prices & Packages
-            </Link>
-            <Link href="/faqs" className="">
-              FAQs
-            </Link>
-            <Link href="/reviews" className="">
-              Reviews
-            </Link>
-            <Link href="/contactUs" className="">
-              Contact Us
-            </Link>
-          </div>
-        </div>
+const Header = () => {
+  return (
+    <div className="header-pointing-logo">
+      <div className="top-contact-link">
+        0419 421 848 info@keesydrivingschool.com.au
       </div>
-    );
-  }
-}
+      <Link to="/" className="item logo">
+        <img src={logo} alt="logo" />
+      </Link>
+      <div className="header-right-menu">
+        <Link to="/" className="item">
+          Home
+        </Link>
+        <Link to="/aboutUs" className="item">
+          About Us
+        </Link>
+        <Link to="/prices&packages" className="item">
+          Prices & packages
+        </Link>
+        <Link to="/faqs" className="item">
+          FAQs
+        </Link>
+        <Link to="/reviews" className="item">
+          Reviews
+        </Link>
+        <Link to="/contactUs" className="item">
+          Contact Us
+        </Link>
+      </div>
+    </div>
+  );
+};
+
 export default Header;
