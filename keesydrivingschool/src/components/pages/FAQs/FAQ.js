34cfd6cdf6b46667aae3de089e2d43164e507653
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-12-24 07:47:11 +1100

    Working with Admin FAQ, Creatw FAQ

---
+++ keesydrivingschool/src/components/pages/FAQs/FAQ.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class FAQ extends Component {
+  render() {
+    return (
+      <div className="onefaq">
+        <h4 className="onefaq-parts">{this.props.faq.question}</h4>
+        <p className="onefaq-parts">{this.props.answer}</p>
+      </div>
+    );
+  }
+}
+
+export default FAQ;
