e98eb785c1418c39da510ad07739615c10f18bb3
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2021-01-06 05:41:24 +1100

    Add test for component AdminControlNav

---
+++ src/tests/components/AdminControlNav.test.js
@@ -0,0 +1,32 @@
+import AdminControlNav from "../../components/AdminControlNav";
+
+import { shallow } from "enzyme";
+
+describe("Admin Control navigation", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<AdminControlNav />);
+  });
+  test("renders admin Faq link", () => {
+    expect(wrapper.find(".admin-single-button").get(0).props.children).toBe(
+      "Admin FAQs"
+    );
+    expect(wrapper.find(".admin-single-button").get(0).props.to).toBe(
+      "/adminFAQ"
+    );
+  });
+  test("renders admin prices & packages link", () => {
+    expect(wrapper.find(".admin-single-button").get(1).props.children).toBe(
+      "Admin Prices & Packages"
+    );
+    expect(wrapper.find(".admin-single-button").get(1).props.to).toBe(
+      "/adminPrice"
+    );
+  });
+  test("renders admin logout link", () => {
+    expect(wrapper.find(".admin-single-button").get(2).props.children).toBe(
+      "Log out"
+    );
+    expect(wrapper.find(".admin-single-button").get(2).props.to).toBe("/");
+  });
+});
