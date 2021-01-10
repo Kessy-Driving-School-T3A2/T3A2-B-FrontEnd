cc2e9ea5271083bf253a44ee2714f36815cddaf1
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2021-01-11 10:47:40 +1100

    Price(user page) css responsive

--- src/components/pages/Price_user.js
+++ src/components/pages/Price_user.js
@@ -1,11 +1,36 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Price_user extends Component {
   render() {
+    const priceBox = {
+      backgroundColor: "#F17B2B",
+      width: "250px",
+      height: "auto",
+      textAlign: "center",
+      padding: "20px",
+      marginBottom: "30px"
+    };
+    const price = {
+      color: "black",
+      padding: "0 0 15px 0",
+      fontWeight: "900"
+    };
+    const booknow = {
+      color: "black",
+      border: "1px solid black",
+      padding: "5px 8px"
+    };
     return (
-      <div className="onePrice">
-        <div className="onePrice-body-parts onePrice-parts">
-          {this.props.price.lesson} : $ {this.props.price.price}
+      <div className="onePrice" style={{ marginBottom: "10px" }}>
+        <div style={priceBox}>
+          <p style={{ color: "black", padding: "0" }}>
+            {this.props.price.lesson}
+          </p>
+          <h1 style={price}>${this.props.price.price}</h1>
+          <Link to="/contact" style={booknow}>
+            Book Now
+          </Link>
         </div>
       </div>
     );
