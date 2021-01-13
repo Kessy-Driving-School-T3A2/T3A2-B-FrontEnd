aa137ee6acedbf6183ad6465885a09d77a7d47bf
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2021-01-13 23:00:12 +1100

    edit price done

--- src/components/pages/Price.js
+++ src/components/pages/Price.js
@@ -1,25 +1,64 @@
 import React, { Component } from "react";
+import EditPrice from "../pages/Prices/EditPrice";
 
-class Price_user extends Component {
-  render() {
+class Price extends Component {
+  constructor(props) {
+    super(props);
+    //create state to hold items in an empty array
+    this.state = {
+      isEditMode: false
+    };
+  }
+
+  handleEdit = () => {
+    this.setState({
+      isEditMode: !this.state.isEditMode
+    });
+  };
+
+  renderPrice = () => {
     return (
       <div className="onePrice">
         <div className="onePrice-body-parts onePrice-parts">
           {this.props.price.lesson} : $ {this.props.price.price}
         </div>
-        <button
-          style={{
-            backgroundColor: "orange",
-            border: "none",
-            padding: "10px",
-            margin: "10px 30px"
-          }}
-        >
-          Edit
-        </button>
+      </div>
+    );
+  };
+
+  handleUpdatedPrices = () => {
+    this.setState({ isEditMode: false });
+    this.props.handleFetchUpdatedPrices();
+  };
+
+  render() {
+    const price_button = {
+      backgroundColor: "orange",
+      border: "none",
+      padding: "10px",
+      margin: "10px 30px"
+    };
+
+    return (
+      <div className="onePrice">
+        <div className="onePrice-body-parts">
+          {this.state.isEditMode ? (
+            <EditPrice
+              id={this.props.price._id}
+              lesson={this.props.price.lesson}
+              price={this.props.price.price}
+              fetchUpdatedPrices={this.handleUpdatedPrices}
+            />
+          ) : (
+            this.renderPrice()
+          )}
+          <button style={price_button} onClick={this.handleEdit}>
+            {this.state.isEditMode ? "Cancel" : "Edit"}
+          </button>
+        </div>
       </div>
     );
   }
 }
 
-export default Price_user;
+export default Price;
